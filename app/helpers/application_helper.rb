ecc9e27834bce122d830a6f9885df9b0690c0f2d
Wolfgang Barth <wob@swobspace.net>
2020-09-27 11:22:38 +0200

    layout and shared views, basic stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
   include Wobapphelpers::Helpers::All
+
+  def configuration_active_class
+    if Titracka::CONFIGURATION_CONTROLLER.include?(controller.controller_name.to_s)
+      "active"
+    end
+  end
 end
