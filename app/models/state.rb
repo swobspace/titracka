95a704c226b578f010b27c2c26f3bd8b160e2a52
Wolfgang Barth <wob@swobspace.net>
2020-10-04 13:16:46 +0200

    complete state model

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,15 @@
 class State < ApplicationRecord
+  # -- associations
+  has_many :tasks, dependent: :restrict_with_error
+
+  # -- configuration
+  STATES = [ "pre", "open", "done", "archive" ]
+  # -- validations and callbacks
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :state, inclusion: STATES, allow_blank: false
+
+  def to_s
+    "#{name}"
+  end
+
 end
