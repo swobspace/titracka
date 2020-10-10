ef964f88097323ccbbe9d7a8bc8905b809c9937e
Wolfgang Barth <wob@swobspace.net>
2020-10-10 18:04:04 +0200

    transactional_fixtures = false

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,7 +51,7 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
