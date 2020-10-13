f8a55c309d6e48b207ec400465a771a9acd1857f
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2020-10-13 10:02:06 +0200

    add more deployment stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :deploy_to, config['deploy_to']
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
 
-append :linked_files, "config/database.yml", "config/titracka.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/titracka.yml", "config/secrets.yml", "config/master.key", "config/credentials.yml.enc"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
