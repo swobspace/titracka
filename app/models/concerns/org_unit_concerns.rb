c4532d549c81bb39e8b21746a0a74d6c9e13a0c5
Wolfgang Barth <wob@swobspace.net>
2020-10-18 12:37:26 +0200

    starting org_unit_concerns: arrange_as_array

---
+++ app/models/concerns/org_unit_concerns.rb
@@ -0,0 +1,22 @@
+module OrgUnitConcerns
+  extend ActiveSupport::Concern
+  NBSP = "\xC2\xA0"
+  SUBTREE = "\xe2\x94\x94"
+  ARROW = [8594].pack('U*')
+
+  included do
+    def self.arrange_as_array(options={}, hash=nil)
+      hash ||= arrange(options)
+      arr = []
+      hash.each do |node, children|
+        arr << node
+        arr += arrange_as_array(options, children) unless children.empty?
+      end
+      arr
+    end
+  end
+
+  def name_for_selects
+    "#{NBSP * depth * 2}#{SUBTREE unless depth == 0}#{name}"
+  end
+end
---
+++ spec/models/concerns/org_unit_concerns_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe OrgUnitConcerns do
+  let!(:ou1)    { FactoryBot.create(:org_unit, name: "ou1") }
+  let!(:ou2)    { FactoryBot.create(:org_unit, name: "ou2") }
+  let!(:ou3)    { FactoryBot.create(:org_unit, name: "ou3") }
+  let!(:ou11)   { FactoryBot.create(:org_unit, parent: ou1, name: "ou11") }
+  let!(:ou12)   { FactoryBot.create(:org_unit, parent: ou1, name: "ou12") }
+  let!(:ou13)   { FactoryBot.create(:org_unit, parent: ou1, name: "ou13") }
+  let!(:ou121)  { FactoryBot.create(:org_unit, parent: ou12, name: "ou121") }
+  let!(:ou122)  { FactoryBot.create(:org_unit, parent: ou12, name: "ou122") }
+  let!(:ou123)  { FactoryBot.create(:org_unit, parent: ou12, name: "ou123") }
+  let!(:ou131)  { FactoryBot.create(:org_unit, parent: ou13, name: "ou131") }
+  let(:arrow)   { " #{[8594].pack('U*')} " }
+
+  describe "#arrange_as_array" do
+    it "orders children with same parent" do
+      ou3.update(position: 1)
+      ou11.update(position: 3)
+      ou123.update(position: 1)
+      expect(OrgUnit.all.arrange_as_array(order: 'position').pluck(:id).to_a).to eq(
+        [ou3, ou1, ou12, ou123, ou121, ou122, ou13, ou131, ou11, ou2].map(&:id)
+      )
+    end
+  end
+end
