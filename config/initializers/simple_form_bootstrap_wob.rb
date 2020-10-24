b2183ef17d07671a5c0a7eb9999f8621ec52402c
Wolfgang Barth <wob@swobspace.net>
2020-10-24 17:11:31 +0200

    add wrapper horizontal_select2 for stimulus-controller

--- config/initializers/simple_form_bootstrap_wob.rb
+++ config/initializers/simple_form_bootstrap_wob.rb
@@ -20,4 +20,19 @@ SimpleForm.setup do |config|
       ba.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
     end
   end
+
+  config.wrappers :horizontal_select2, tag: 'div', class: 'form-group row', error_class: 'form-group-invalid', valid_class: 'form-group-valid', html: {data: {controller: "select2" }} do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :pattern
+    b.optional :readonly
+    b.use :label, class: 'col-sm-3 form-control-label'
+
+    b.wrapper :grid_wrapper, tag: 'div', class: 'col-sm-9' do |ba|
+      ba.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid', data: {target: 'select2.input'}
+      ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
+      ba.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+    end
+  end
+
 end
