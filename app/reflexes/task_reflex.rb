d9a278f7b26c06068c59f906e627af5d4ae1b24e
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2020-11-06 14:00:28 +0100

    use morphs for partial rerendering

--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -3,6 +3,9 @@
 class TaskReflex < ApplicationReflex
   def show(task_id)
     @task = Task.find(task_id)
+    morph "#showTaskModalTable", TasksController.render(
+      partial: 'tasks/show_task', locals: { task: @task }
+    )
   end
 
 end
