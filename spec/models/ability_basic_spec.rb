45168631bd4e229309e10e17682b90ca19eaad3a
Wolfgang Barth <wob@swobspace.net>
2020-11-07 17:30:19 +0100

    remove dashboard

--- spec/models/ability_basic_spec.rb
+++ spec/models/ability_basic_spec.rb
@@ -5,8 +5,7 @@ configuration_models =
   Titracka::CONFIGURATION_CONTROLLER.map{|c| c.singularize.camelize.constantize}
 
 data_models = [ List, Task, TimeAccounting, Workday ]
-
-navigation = [:dashboard, :lists, :tasks, :time_accountings, :configuration]
+navigation = [:lists, :tasks, :time_accountings, :configuration]
 
 RSpec.describe "User", :type => :model do
   fixtures 'wobauth/roles'
