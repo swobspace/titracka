f36ef8766239e48de23e575e2736ec4cd897d7b3
Wolfgang Barth <wob@swobspace.net>
2020-11-08 11:26:30 +0100

    switch cache and session store to redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
-Rails.application.config.session_store :active_record_store,
+# Rails.application.config.session_store :active_record_store,
+#                                        key: '_titracka_session',
+#                                        expire_after: 4.weeks
+Rails.application.config.session_store :cache_store,
                                        key: '_titracka_session',
-                                       expire_after: 2.weeks
+                                       compress: true, 
+                                       pool_size: 5,
+                                       expire_after: 1.year
