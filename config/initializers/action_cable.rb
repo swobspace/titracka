0075b5232bf716e4d8bba930fef4d706dc398642
Wolfgang Barth <wob@swobspace.net>
2020-11-13 18:57:09 +0100

    fix typo§

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -1,3 +1,3 @@
 if Rails.env.production? and Titracka.action_cable_allowed_request_origins
-  config.action_cable.allowed_request_origins = Titracka.action_cable_allowed_request_origins
+  Rails.application.config.action_cable.allowed_request_origins = Titracka.action_cable_allowed_request_origins
 end
