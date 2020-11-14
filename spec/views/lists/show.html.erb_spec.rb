4999eb377dbc9e21e4b1e3eefd3d5e42bf66821e
Wolfgang Barth <wob@swobspace.net>
2020-11-14 18:01:10 +0100

    update spec

--- spec/views/lists/show.html.erb_spec.rb
+++ spec/views/lists/show.html.erb_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "lists/show", type: :view do
+  let!(:state) { FactoryBot.create(:state, :open) }
   before(:each) do
     @ability = Object.new
     @ability.extend(CanCan::Ability)
     allow(controller).to receive(:current_ability) { @ability }
     allow(controller).to receive(:controller_name) { "lists" }
     allow(controller).to receive(:action_name) { "show" }
-
     org_unit = FactoryBot.create(:org_unit, name: "ACME")
 
     @list = assign(:list, FactoryBot.create(:list,
       org_unit: org_unit,
       name: "Name"
     ))
+
+    @columns = [state]
+    @tasks_per_column = []
   end
 
   it "renders attributes in <p>" do
