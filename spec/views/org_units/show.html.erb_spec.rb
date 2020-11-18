b14fd63651f3a77f4cdd58a8392f1a32abad0651
Wolfgang Barth <wob@swobspace.net>
2020-11-18 17:39:57 +0100

    fix spec

--- spec/views/org_units/show.html.erb_spec.rb
+++ spec/views/org_units/show.html.erb_spec.rb
@@ -8,17 +8,16 @@ RSpec.describe "org_units/show", type: :view do
     allow(controller).to receive(:controller_name) { "org_units" }
     allow(controller).to receive(:action_name) { "index" }
 
-    parent = FactoryBot.create(:org_unit, name: "ACME")
+    parent = FactoryBot.create(:org_unit, name: "Parent Org")
 
     @org_unit = assign(:org_unit, OrgUnit.create!(
-      name: "Name",
+      name: "ACME Ltd.",
       parent_id: parent.id
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/ACME/)
+    expect(rendered).to match(/ACME Ltd./)
   end
 end
