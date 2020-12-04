6b32737fdb4f6976ba1fbcbffc625a6d692355a5
Wolfgang Barth <wob@swobspace.net>
2020-12-04 17:45:18 +0100

    order tasks by ou, list and subject

--- app/decorators/task_decorator.rb
+++ app/decorators/task_decorator.rb
@@ -1,16 +1,9 @@
 class TaskDecorator < Draper::Decorator
   delegate_all
+  ARROW = [8594].pack('U*')
 
   def name_with_list_or_org_unit
-    if object.org_unit.present? && object.list.present?
-      "#{object.org_unit.to_s} / #{object.list.to_s} / #{object.subject}"
-    elsif object.org_unit.present?
-      "#{object.org_unit.to_s} / #{object.subject}"
-    elsif object.list.present?
-      "#{object.list.to_s} / #{object.subject}"
-    else
-      "#{object.subject}"
-    end
+    [object.org_unit, object.list, object.subject].compact.map(&:to_s).join(" / ")
   end
 
 end
