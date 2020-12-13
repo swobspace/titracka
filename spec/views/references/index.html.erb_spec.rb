f73a37629482d501df0374c67c9ac32d1cf7a685
Wolfgang Barth <wob@swobspace.net>
2020-12-13 11:22:08 +0100

    complete references views

--- spec/views/references/index.html.erb_spec.rb
+++ spec/views/references/index.html.erb_spec.rb
@@ -2,21 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "references/index", type: :view do
   before(:each) do
-    assign(:references, [
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { "references" }
+    allow(controller).to receive(:action_name) { "show" }
+
+    @reference = assign(:references, [
       Reference.create!(
-        name: "Name",
-        identifier_name: "Identifier Name"
+        name: "DontDoIt",
+        identifier_name: "Ticketnumber",
+        reference_urls_attributes: [
+          name: "DDI/Web",
+          url: "https://ddi.none/local/###IDENTIFIER###/"
+        ]
       ),
       Reference.create!(
-        name: "Name",
-        identifier_name: "Identifier Name"
+        name: "DontDoIt2",
+        identifier_name: "Ticketnumber",
+        reference_urls_attributes: [
+          name: "DDI/Web",
+          url: "https://ddi.none/local/###IDENTIFIER###/"
+        ]
       )
     ])
   end
 
   it "renders a list of references" do
     render
-    assert_select "tr>td", text: "Name".to_s, count: 2
-    assert_select "tr>td", text: "Identifier Name".to_s, count: 2
+    assert_select "tr>td", text: "DontDoIt".to_s, count: 1
+    assert_select "tr>td", text: "DontDoIt2".to_s, count: 1
+    assert_select "tr>td", text: "Ticketnumber".to_s, count: 2
+    assert_select "tr>td", text: "DDI/Web [ https://ddi.none/local/###IDENTIFIER###/ ]".to_s, count: 2
   end
 end
--- spec/views/references/new.html.erb_spec.rb
+++ spec/views/references/new.html.erb_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "references/new", type: :view do
   before(:each) do
-    assign(:reference, Reference.new(
-      name: "MyString",
-      identifier_name: "MyString"
-    ))
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { "references" }
+    allow(controller).to receive(:action_name) { "new" }
+
+    @reference = assign(:reference, FactoryBot.build(:reference))
   end
 
   it "renders new reference form" do
     render
 
     assert_select "form[action=?][method=?]", references_path, "post" do
-
       assert_select "input[name=?]", "reference[name]"
-
       assert_select "input[name=?]", "reference[identifier_name]"
     end
   end
