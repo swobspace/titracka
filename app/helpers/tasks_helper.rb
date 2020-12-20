5dfb8db983ddcf14f46c1b6eccd8ceaf0716c32e
Wolfgang Barth <wob@swobspace.net>
2020-12-20 14:22:42 +0100

    styling timline and action buttons

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -6,4 +6,16 @@ module TasksHelper
     end
     output.html_safe
   end
+
+  def timeline(task)
+    output = ""
+    phase = task.decorate.phase
+    # if task.start.present?
+    #   output += %Q[<span class="start #{phase} mr-2"><i class="fas fa-calendar-alt"></i> #{task.start}</span>]
+    # end
+    if task.deadline.present?
+      output += %Q[<span class="deadline #{phase}"><i class="fas fa-flag-checkered"></i> #{task.deadline}</span>]
+    end
+    output.html_safe
+  end
 end
--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -4,12 +4,20 @@ require "cancan/matchers"
 RSpec.describe TasksHelper, :type => :helper do
   include Devise::Test::ControllerHelpers
   let(:responsible) { FactoryBot.create(:user, sn: "Mustermann", givenname: "Max") }
-  let(:task) { FactoryBot.create(:task, responsible: responsible) }
 
   describe "#raci" do
+    let(:task) { FactoryBot.create(:task, responsible: responsible) }
     subject { Capybara.string(helper.raci(task)) }
     it "shows responsible enclosed in <span>" do
       expect(subject.find("span.responsible").text).to match("Mustermann, Max")
+    end
+  end
+
+  describe "#timeline" do
+    subject { Capybara.string(helper.timeline(task)) }
+    describe "only start set" do
+      let(:task) { FactoryBot.create(:task, :open, start: 1.day.before(Date.today)) }
+      it { expect(subject.find("span.active").text).to match(1.day.before(Date.today.to_s)) }
     end
   end
 end
