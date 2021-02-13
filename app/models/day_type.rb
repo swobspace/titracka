d3e79399d5530df44ccec46bf88a67d00c1f85ff
Wolfgang Barth <wob@swobspace.net>
2021-02-13 11:40:46 +0100

    get model running

--- app/models/day_type.rb
+++ app/models/day_type.rb
@@ -1,2 +1,13 @@
 class DayType < ApplicationRecord
+  # -- associations
+  has_many :workdays, dependent: :restrict_with_error
+
+  # -- configuration
+  # -- validations and callbacks
+  validates :abbrev, presence: true, uniqueness: { case_sensitive: false }
+
+  def to_s
+    "#{abbrev}"
+  end
+
 end
