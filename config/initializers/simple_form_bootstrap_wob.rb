79e2954e97196bcdd05d113f79d8d5b5be196641
Wolfgang Barth <wob@swobspace.net>
2021-12-26 14:05:53 +0100

    update simple form bootstrap wob initializer; create horizontal_short for short inputs

--- config/initializers/simple_form_bootstrap_wob.rb
+++ config/initializers/simple_form_bootstrap_wob.rb
@@ -1,21 +1,21 @@
 SimpleForm.setup do |config|
-  config.wrappers :horizontal_date, tag: 'div', class: 'form-group row', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
+  config.wrappers :horizontal_date, tag: 'div', class: 'mb-3 row', error_class: 'form-group-invalid', valid_class: 'form-group-valid' do |b|
     b.use :html5
     b.use :placeholder
     b.optional :pattern
     b.optional :readonly
     b.use :label, class: 'col-sm-3 form-control-label'
 
-    b.wrapper :grid_wrapper, tag: 'div', class: 'col-sm-9' do |ba|
-      ba.wrapper tag: 'div', class: 'd-flex flex-row justify-content-between align-items-center' do |bai|
-        bai.use :input, class: 'form-control col-sm-3 date', type: 'date', autocomplete: 'off'
-      end
-      ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
+    b.wrapper :grid_wrapper, tag: 'div', class: 'col-sm-3' do |ba|
+      ba.use :input, class: 'form-control', type: 'date', autocomplete: 'off', error_class: 'is-invalid', valid_class: 'is-valid'
+
+      ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
       ba.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+
     end
   end
 
-  config.wrappers :horizontal_select2, tag: 'div', class: 'form-group row', error_class: 'form-group-invalid', valid_class: 'form-group-valid', html: {data: {controller: "select2" }} do |b|
+  config.wrappers :horizontal_select2, tag: 'div', class: 'mb-3 row', error_class: 'form-group-invalid', valid_class: 'form-group-valid', html: {data: {controller: "select2" }} do |b|
     b.use :html5
     b.use :placeholder
     b.optional :pattern
@@ -23,8 +23,27 @@ SimpleForm.setup do |config|
     b.use :label, class: 'col-sm-3 form-control-label'
 
     b.wrapper :grid_wrapper, tag: 'div', class: 'col-sm-9' do |ba|
-      ba.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid', data: {"select2-target": 'input'}
-      ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback d-block' }
+      ba.use :select, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid', data: {"select2-target": 'input'}
+      ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+      ba.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+
+    end
+  end
+
+  config.wrappers :horizontal_short, tag: 'div', class: 'mb-3 row', 
+                  error_class: 'form-group-invalid', 
+                  valid_class: 'form-group-valid' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label, class: 'col-sm-3 col-form-label'
+    b.wrapper :grid_wrapper, tag: 'div', class: 'col-sm-3' do |ba|
+      ba.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+      ba.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
       ba.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
     end
   end
