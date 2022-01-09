9fd2d4ac8c82754a38cd0692ef547da34ef9087a
Wolfgang Barth <wob@swobspace.net>
2022-01-09 18:34:14 +0100

    remove unused flexes

--- app/reflexes/application_reflex.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ApplicationReflex < StimulusReflex::Reflex
-  delegate :current_user, to: :connection
-
-  def current_ability
-    @current_ability ||= Ability.new(current_user)
-  end
-end
