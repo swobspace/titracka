9ed51aa764f1f47e3c3e0bd929e6559ad493f97f
Wolfgang Barth <wob@swobspace.net>
2022-01-16 09:38:22 +0100

    cleanup unused value

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "stimulus"
 import SlimSelect from "slim-select"
 
 export default class extends Controller {
-  static values = { url: String }
-
   connect() {
     new SlimSelect({
       select: this.element,
