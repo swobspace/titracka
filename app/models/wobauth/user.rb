ad29b0c4ddf1461f8a588075f29c4e9fe45d21d7
Wolfgang Barth <wob@swobspace.net>
2022-12-18 18:26:25 +0100

    update Wobauth::User for rails 7.0

--- app/models/wobauth/user.rb
+++ app/models/wobauth/user.rb
@@ -1,21 +1,22 @@
 require_dependency 'wobauth/concerns/models/user_concerns'
-class Wobauth::User < ActiveRecord::Base
-  has_many :tasks, dependent: :restrict_with_error
-  has_many :lists, dependent: :restrict_with_error
-  has_many :time_accountings, dependent: :restrict_with_error
-  has_many :notes, dependent: :delete_all
-  has_many :responsibilities, dependent: :restrict_with_error
-  has_many :workdays, dependent: :restrict_with_error
-  # dependencies within wobauth models
-  include UserConcerns
+module Wobauth
+  class User < ApplicationRecord
+    has_many :tasks, dependent: :restrict_with_error
+    has_many :lists, dependent: :restrict_with_error
+    has_many :time_accountings, dependent: :restrict_with_error
+    has_many :notes, dependent: :delete_all
+    has_many :responsibilities, dependent: :restrict_with_error, class_name: 'Task', foreign_key: :responsible_id
+    has_many :workdays, dependent: :restrict_with_error
+    # dependencies within wobauth models
+    include UserConcerns
 
-  # devise *#{@app_name}.devise_modules 
-  # or ... basic usage:
-  devise *Titracka.devise_modules
+    # devise *#{@app_name}.devise_modules 
+    # or ... basic usage:
+    devise *Titracka.devise_modules
 
-  validates :password, confirmation: true
+    validates :password, confirmation: true
 
 
-  scope :active, -> { all }
+    scope :active, -> { all }
+  end
 end
-
