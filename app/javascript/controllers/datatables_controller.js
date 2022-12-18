a0ea4838db97a3f66a2e6125c04e0e33bf0ee296
Wolfgang Barth <wob@swobspace.net>
2022-12-18 11:03:29 +0100

    changes for datatables 1.13.1

--- app/javascript/controllers/datatables_controller.js
+++ app/javascript/controllers/datatables_controller.js
@@ -1,38 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
-// jQuery and esbuild are not the best friends
-import '../src/jquery.js'
-import 'pdfmake'
-import dataTable from 'datatables.net-bs5'
-dataTable(window, $)
-import buttons from 'datatables.net-buttons-bs5'
-buttons(window, $)
-import columnVisibility from 'datatables.net-buttons/js/buttons.colVis.js'
-columnVisibility(window, $)
-import buttonsHtml5 from 'datatables.net-buttons/js/buttons.html5.js'
-buttonsHtml5(window, $)
-import buttonsPrint from 'datatables.net-buttons/js/buttons.print.js'
-buttonsPrint(window, $)
+import '../src/datatables-bs5'
 
-import { DataTable } from "datatables.net"
-
-// here comes the controller ...
 export default class extends Controller {
+  static values = {
+    simple: Boolean,
+    url: String
+  }
 
   initialize() {
-    var myTable = $.fn.dataTable;
-    $.extend( true, myTable.Buttons.defaults, {
-      "dom": { "button": { "className": 'btn btn-outline-secondary btn-sm' } }
-    })
   }
 
   connect() {
-    this.setInputFields()
+    let dtOptions = {}
+    this.compileOptions(dtOptions)
+    if (!this.simpleValue) {
+      this.setInputFields()
+    }
     const table = $(this.element.querySelector('table'))
 
     // prepare options, optional add remote processing (not yet implemented)
-    let options = Object.fromEntries(this.dtOptions())
-    let dtable = $(table).DataTable(options)
+    let dtable = $(table).DataTable(dtOptions)
 
     // process search input
     dtable.columns().eq(0).each((colIdx) => {
@@ -44,9 +32,10 @@ export default class extends Controller {
 
   // search fields for each column
   setInputFields() {
-    this.element.querySelectorAll('table tfoot th').forEach((th, idx) => {
-      th.insertAdjacentHTML('afterbegin', this.searchField(idx))
-    })
+    this.element.querySelectorAll("table tfoot th:not([class='nosearch'])")
+        .forEach((th, idx) => {
+          th.insertAdjacentHTML('afterbegin', this.searchField(idx))
+        })
   }
 
   // single search input field
@@ -55,31 +44,61 @@ export default class extends Controller {
   }
 
   // datatables options
-  dtOptions() {
-    const options = new Map()
-    options.set("pagingType", "full_numbers")
-    options.set("dom", "<'row'<'col'l><'col'B><'col'f>>" +
-                       "<'row'<'col-sm-12'tr>>" +
-                       "<'row'<'col'i><'col'p>>")
-    options.set("stateSave", false)
-    options.set("lengthMenu", [ [10, 25, 100, 250, 1000], [10, 25, 100, 250, 1000] ])
-    options.set("buttons", [ { "extend": 'copy',
-	                       "exportOptions": {
-	                         "columns": ':visible',
-	                         "search": ':applied' } },
-                             { "extend": 'excelHtml5',
-	                       "exportOptions": { "search": ':applied' } },
-                             { "extend": 'pdf',
-	                       "orientation": 'landscape',
-	                       "pageSize": 'A4',
-	                       "exportOptions": { "columns": ':visible',
-	                                          "search": ':applied' } },
-                             { "extend": 'print'},
-                             { "extend": 'colvis',
-	                       "columns": ':gt(0)' } ])
-    options.set("columnDefs", [ { "targets": "nosort", "orderable": false },
-                                { "targets": "notvisible", "visible": false } ])
-    // return json object for datatables
-    return options
+  compileOptions(options) {
+    // common options
+    options.pagingType = "full_numbers"
+    options.stateSave = false
+    options.lengthMenu = [ [10, 25, 100, 250, 1000], [10, 25, 100, 250, 1000] ]
+    options.columnDefs = [ { "targets": "nosort", "orderable": false },
+                           { "targets": "notvisible", "visible": false },
+                           { "targets": "actions", "className": "actions" } ]
+    // with or without buttons
+    if (this.simpleValue) {
+      this.simpleOptions(options)
+    } else {
+      this.buttonOptions(options)
+    }
+
+    // remote fetch via ajax or plain html data
+    if (this.hasUrlValue) {
+      this.remoteOptions(options)
+    }
+  }
+
+  simpleOptions(options) {
+    options.dom =  "<'row'<'col-sm-12'tr>>" +
+                   "<'row'<'col pt-2'l><'col'i><'col'p>>"
+    options.pagingType = "numbers"
+  }
+
+
+  buttonOptions(options) {
+    options.dom = "<'row'<'col'l><'col'B><'col'f>>" +
+                    "<'row'<'col-sm-12'tr>>" +
+                    "<'row'<'col'i><'col'p>>"
+    options.buttons = {
+      dom: {
+        button: {
+          tag: 'button',
+          className: 'btn btn-outline-secondary btn-sm'
+        }
+      },
+      buttons: [ { "extend": 'excel',
+	           "exportOptions": { "search": ':applied' } },
+                 { "extend": 'pdf',
+	           "orientation": 'landscape',
+	           "pageSize": 'A4',
+	           "exportOptions": { "columns": ':visible',
+	                              "search": ':applied' } },
+                 { "extend": 'print'},
+                 { "extend": 'colvis', "columns": ':gt(0)' } ]
+    }
+  }
+
+  remoteOptions(options) {
+    options.searchDelay = 400
+    options.processing = true
+    options.serverSide = true
+    options.ajax = { "url": this.urlValue, "type": "POST" }
   }
 } // Controller
