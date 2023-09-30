81a18fc427e9d4e16e3c25d904f069bc19de3e80
Wolfgang Barth <wob@swobspace.net>
2023-09-30 17:46:35 +0200

    catch tasks without ou or list

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Dashboard", type: :feature do
   before(:each) do
     login_user(user: mcaro, org_unit: ou1, role: 'Manager')
     visit root_path
-    execute_script("$.support.transition = false")
+    # execute_script("$.support.transition = false")
   end
 
   describe "visit root_path", js: true do
